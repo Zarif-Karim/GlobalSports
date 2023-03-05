75d0753edb443a6906ae01ea6f58f1adac27c5ce
Zarif Karim <zarifkarim09@gmail.com>
2023-03-05 12:39:27 +1100

    installed typescript, going through docs -> The Basics

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+ 
+greet("Brendan", Date.now());
+greet("Zarif", Date.now());
+
